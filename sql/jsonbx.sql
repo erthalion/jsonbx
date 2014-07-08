aa67024b7cf16c8565d65df433476efa7db204e1
erthalion <9erthalion6@gmail.com>
2014-07-08 23:31:15 +0700

    Test for jsonb_concat

--- sql/jsonbx.sql
+++ sql/jsonbx.sql
@@ -1,3 +1,4 @@
 CREATE EXTENSION jsonbx;
 
 select jsonb_print('{"a": "test", "b": [1, 2, 3], "c": "test3", "d":{"dd": "test4", "dd2":{"ddd": "test5"}}}'::jsonb, true);
+select jsonb_concat('{"d": "test", "a": [1, 2]}'::jsonb, '{"g": "test2", "c": {"c1":1, "c2":2}}'::jsonb);
